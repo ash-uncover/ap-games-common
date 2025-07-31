2d6e07d2ef8fc305fb276bf95907a456d14a1903
ash-uncover <ash.uncover@gmail.com>
2025-07-31 14:47:02 +0200

    clean dist

--- dist/components/panel/Panel.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import './Panel.css';
-export interface PanelProperties extends PropsWithChildren {
-    className?: string;
-    title?: string;
-}
-export declare const Panel: ({ className, title, children, }: PanelProperties) => React.JSX.Element;
--- dist/components/panel/PanelButton.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-import './PanelButton.css';
-export interface PanelButtonProperties {
-    className?: string;
-    title?: string;
-    children: ReactNode;
-    onClick: () => void;
-}
-export declare const PanelButton: ({ className, title, children, onClick }: PanelButtonProperties) => React.JSX.Element;
